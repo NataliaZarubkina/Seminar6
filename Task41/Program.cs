e4dd25dd0a41632ed6513a33f250ddeaa11e4151
Natalia Zarubkina <tasha3425@mail.ru>
2023-03-19 17:17:13 +0300

    add task 41.43

---
+++ Task41/Program.cs
@@ -0,0 +1,55 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.Clear();
+int userQantity = GetQantityFromUser("Введите количество элементов: ", "Ошибка ввода!");
+int[] array = FillArrayFromUser("Введите целое число: ", "Ошибка ввода!", userQantity);
+int result = PositiveNumber(array);
+Console.WriteLine($"{String.Join(", ", array)} -> {result}");
+
+int GetQantityFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+int[] FillArrayFromUser(string message, string errorMessage, int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = GetNumberFromUser("Введите элемент: ", "Ошибка ввода!");
+
+    }
+return array;
+}
+
+
+int PositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+double k1 = GetNumberFromUser("Введите k1 : ", "Ошибка ввода!");
+double b1 = GetNumberFromUser("Введите b1 : ", "Ошибка ввода!");
+double k2 = GetNumberFromUser("Введите k2 : ", "Ошибка ввода!");
+double b2 = GetNumberFromUser("Введите b2 : ", "Ошибка ввода!");
+double x = FindX(k1, k2, b1, b2);
+double y = FindY(k1, k2, b1, b2, x);
+Console.WriteLine($"k1={k1}, b1={b1}, k2={k2}, b2={b2} -> ({x};{y})");
+
+
+double FindX(double k1, double k2, double b1, double b2)
+{
+    double result = (b2 - b1) / (k1 - k2);
+    return result;
+}
+
+double FindY(double k1, double k2, double b1, double b2, double x)
+{
+    double result = k1 * (b2 - b1) / (k1 - k2) + b1;
+    return result;
+}
+
+double GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = double.TryParse(Console.ReadLine(), out double userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
